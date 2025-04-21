32ae6059438ca28df4f0067d2fc338dd45973614
17Arhaan <17arhaan@gmail.com>
2025-04-21 21:47:17 +0530

    commit

--- components/certifications-section.tsx
+++ components/certifications-section.tsx
@@ -176,7 +176,7 @@ export default function CertificationsSection() {
           />
         </motion.div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-20 place-items-center">
+        <div className="grid grid-cols-3 gap-8 place-items-center max-w-[1400px] mx-auto">
           {certificationsData.map((cert, index) => {
             const x = useMotionValue(0)
             const y = useMotionValue(0)
